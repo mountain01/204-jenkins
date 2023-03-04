093ac37c72ee407d0c6366bdcfe6b0c2bdfa1baf
Matt Edwards <mountain01@users.noreply.github.com>
2023-03-03 22:49:37 -0600

    Loop code

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,41 +4,45 @@ public class Main {
   public static void main(String[] args) {
     Scanner in = new Scanner(System.in);
 
-    String func = in.next();
-    StringBuilder builder = new StringBuilder();
+    String func;
     Calculator calc = new Calculator();
     int a, b;
-    switch(func){
-      case "add":
-        a = in.nextInt();
-        b = in.nextInt();
-        builder.append("Output: ").append(calc.add(a,b));
-        break;
-      case "sub":
-        a = in.nextInt();
-        b = in.nextInt();
-        builder.append("Output: ").append(calc.subtract(a,b));
-        break;
-      case "multiply":
-        a = in.nextInt();
-        b = in.nextInt();
-        builder.append("Output: ").append(calc.multiply(a,b));
-        break;
-      case "divide":
-        a = in.nextInt();
-        b = in.nextInt();
-        builder.append("Output: ").append(calc.divide(a,b));
-        break;
-      case "fib":
-        a = in.nextInt();
-        builder.append("Output: ").append(calc.fibonacciNumberFinder(a));
-        break;
-      case "binary":
-        a = in.nextInt();
-        builder.append("Output: ").append(calc.intToBinaryNumber(a));
-        break;
+    System.out.println("Calculator here, I do things");
+    loop: while((func = in.next()) != "q"){
+      switch(func){
+        case "add":
+          a = in.nextInt();
+          b = in.nextInt();
+          System.out.println(calc.add(a, b));
+          break;
+        case "sub":
+          a = in.nextInt();
+          b = in.nextInt();
+          System.out.println(calc.subtract(a,b));
+          break;
+        case "multiply":
+          a = in.nextInt();
+          b = in.nextInt();
+          System.out.println(calc.multiply(a, b));
+          break;
+        case "divide":
+          a = in.nextInt();
+          b = in.nextInt();
+          System.out.println(calc.divide(a, b));
+          break;
+        case "fib":
+          a = in.nextInt();
+          System.out.println(calc.fibonacciNumberFinder(a));
+          break;
+        case "binary":
+          a = in.nextInt();
+          System.out.println(calc.intToBinaryNumber(a));
+          break;
+        default:
+          break loop;
+      }
     }
-    System.out.println(builder.toString());
+    System.out.println("bye");
     in.close();
   }
 }
